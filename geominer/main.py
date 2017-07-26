db6264363bb8a005c227c5f142f7a84d15978e91
axel <axelatcaltech@gmail.com>
2017-07-26 14:45:04 -0700

    single word molecules from chib added to dataframe

--- geominer/main.py
+++ geominer/main.py
@@ -17,6 +17,8 @@ import pandas as pd
 # import enchant
 # d = enchant.Dict("en_US")
 
+import my_funs as mf
+
 
 ### from load_input.py
 #  load a gene ontology (not go) 
@@ -29,27 +31,31 @@ pcg_hs = [i.lower() for i in pcg_hs]
 
 # Reading in diabetes series matrix
 # This tsv file was produced using R and GEO-metadb
-dsm = pd.read_table('/home/axel/projects/hirn/beta_cells/beta_R/nursa_grant/diabetes_series_matrix.tsv') 
+dsm = pd.read_table('/home/axel/projects/hirn/beta_cells/beta_R/nursa_grant/diabetes_series_matrix.tsv')
 gse_summary = dsm[['gse','summary']].drop_duplicates()
 ###
 
-gse_summary = dsm[['gse','summary']].drop_duplicates()
+# tokenize summaries to sentences
+
 gse_summary['sentences'] = gse_summary.summary.apply(nltk.sent_tokenize)
 gse_summary = gse_summary[['gse', 'sentences']]
-gse_sentences = gse_summary.groupby('gse').sentences.apply(lambda x: pd.DataFrame([item for sublist in x.values for item in sublist]))
-gse_sentences.columns = ['sentences']
-gse_sentences['sentences'] = gse_sentences.sentences.apply(nltk.word_tokenize)
 
-gse_sentences['sentences'] = gse_sentences.sentences.apply(nltk.pos_tag)
-
+# create a dataframe with gse and individual sentences
+df_gse_sentences = gse_summary.groupby('gse').sentences.apply(lambda x: pd.DataFrame([item for sublist in x.values for item in sublist]))
+# name index
+df_gse_sentences.index.names =  ['gse', 'sentence_number']
+df_gse_sentences.columns = ['sentence']                                           
+# convert indices to columns
+df_gse_sentences = df_gse_sentences.reset_index(level = ['gse',
+                                           'sentence_number'])
+# apply word tokenization and position tagging
+gse_sentences['sentences'] = df_gse_sentences.sentence.apply(nltk.word_tokenize)
+gse_sentences['sentences'] = df_gse_sentences.sentence.apply(nltk.pos_tag)
 gse_words = gse_sentences.groupby(gse_sentences.index).sentences.apply(lambda x: pd.DataFrame([item for sublist in x.values for item in sublist]))
-
 gse_words.columns = ['word', 'pos_tag']
 
 # convert words to lower case words
 gse_words.word = gse_words.word.str.lower()
-
-
 # the next step is necessary since str.lower() introduced 
 # instance of type <class 'pandas.core.series.Series'>
 gse_words = pd.DataFrame(gse_words)
@@ -70,7 +76,6 @@ df_temp.index.names = ['count']
 df_temp = df_temp.rename(columns = {0: 'gse', 1: 'sentence'})
 
 # join df_temp with gse_words on index, exclude first column
-
 gse_words_temp = df_temp.merge(gse_words[gse_words.columns[1:]], 
                                right_index = True, left_index = True)
 
@@ -113,5 +118,92 @@ df_gse_words['is_english'] = df_gse_words['synsets'].map(lambda x: len(x) > 0)
 # TODO
 # check if word or noun is in other ontologies, start with CHEBI
 
+# Chemical Entities of Biological Interest
+# Use [Chemical Entities of Biological Interest
+# Ontology](http://bioportal.bioontology.org/ontologies/CHEBI) to 
+# identify molecules in summaries
+
+chebi = Ontology("/home/axel/Documents/ontologies/chebi.owl")
+
+chebi_names = []
+for e in chebi:
+    chebi_names.append(e.name)
+
+chebi_ids = []
+for term in chebi:
+    chebi_ids.append(term.id)
+
+molecules_dict = {}
+for i in chebi_ids:
+    molecules_dict.update(mf.get_molecule_names(i))
+
+all_molecules = set([item for sublist in list(molecules_dict.values()) 
+                     for item in sublist])
+
+single_word_molecules = set()
+multi_word_molecules = set()
+
+some_whitespace = re.compile('\w*[\s\W]\w*')
+
+#for i in all_molecules:
+for i in all_molecules:
+    if re.match(some_whitespace, i):
+        multi_word_molecules.update([i])
+    else:
+        single_word_molecules.update([i])
+        
+df_gse_words['word_is_molecule'] = df_gse_words['word'].map(
+    lambda x: x in single_word_molecules)
+
+
+###
+# get noun_phrases(np) from word_features_list(wfl)
+nps_from_wfl = word_features_list
+
+tmp_list = []
+for i in nps_from_wfl[:30]:
+    if i[1] in np_components:
+        tmp_list.append(i[0])
+    else:
+        tmp = " ".join(tmp_list)
+        i.append(tmp)
+        tmp_list = []
+
+
+
+#  word_features_list = [list(elem) for elem in word_features]
+# get noun_phrases(np) from word_features_list(wfl)
+nps_from_wfl = word_features_list
+tmp_list = []
+
+for i in enumerate(nps_from_wfl):
+    if i[1][1]in np_components:
+        tmp_list.append(i[1][0])
+        print("temp_list:", len(tmp_list))
+    else:
+        tmp = " ".join(tmp_list)
+        for elem in range(1, (len(tmp_list) + 1)):
+            print(elem)
+            #print(nps_from_wfl[i[0]-elem])
+            nps_from_wfl[i[0]-elem].append(tmp)
+        tmp_list = []
+
+# check noun phrases for presence of chebi names or synonymes
+
+# duplicate lits for testing purposes
+word_features_list_chebi = word_features_list[:]
+
+# make sure each noun phrase is searched once only:
+test = []
+np_phrase = ["place_holder"]
+for i in enumerate(word_features_list_chebi):
+    if np_phrase[-1] == i[1][-1]:
+        pass
+    else:
+        for names_list in molecules_dict.values():
+            if np_phrase[-1] in names_list:
+                test.append(i[1])
+
+
 # TODO
-# vectorize words with tensorflow
+
